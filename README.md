3d48a4509c571605c8cbf532d2d394e4621cba9f
Morgan Breden <morganbreden@gmail.com>
2026-04-06 20:40:55 -0400

    Fixed some bad typing and poor C understanding

--- README.md
+++ README.md
@@ -6,5 +6,3 @@ My first thought was to convert images to a small binary hash with a discrete co
 Forcing a square aspect ratio prevents distortion tricking the algorithm and gives us a square array every time.
 
 The `topPixels` bit in `imageHash()` is an attempt to deal with cropping/padding.
-
-Currently there's an issue with pointers causing both hashes to be the same, because I haven't touched C in years and am spoiled on high level languages.
--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ const int TRANSFORMED_SIZE = 64;
 
 gdImagePtr readFileAsImage(char * path);
 gdImagePtr resizeImage(gdImagePtr image, int w, int h);
-void oneDimensionalDCT(int* row, double* output);
+void oneDimensionalDCT(double* row, double* output);
 void imageHash(gdImagePtr image, int* bits, int label);
 double getMeanColorFromPixelSet(const double * pixels);
 int hammingDistance(int one[], int two[]);
@@ -19,11 +19,8 @@ void debugDump(int * hash);
 int main (int argc, char *argv[])
 {
     /* Allocate vars */
-    gdImagePtr sourceImage;
-    gdImagePtr compImage;
     int compHash[TRANSFORMED_SIZE];
     int sourceHash[TRANSFORMED_SIZE];
-    int distance;
 
     if(argc < 3) {
         fprintf(stderr, "Error: Not enough arguments.\n");
@@ -36,8 +33,8 @@ int main (int argc, char *argv[])
     /* New file to compare against the source file for a match */
     char *compFilePath = argv[2];
 
-    sourceImage = gdImageCreateFromFile(sourceFilePath);
-    compImage = gdImageCreateFromFile(compFilePath);
+    gdImagePtr sourceImage = gdImageCreateFromFile(sourceFilePath);
+    gdImagePtr compImage = gdImageCreateFromFile(compFilePath);
 
     /* Handle invalid file paths */
     if(sourceImage == NULL) {
@@ -64,7 +61,7 @@ int main (int argc, char *argv[])
     imageHash(compImage, compHash, 1);
     imageHash(sourceImage, sourceHash, 2);
 
-    distance = hammingDistance(compHash, sourceHash);
+    int distance = hammingDistance(compHash, sourceHash);
 
     fprintf(stdout, "Distance: %d\n", distance);
 
@@ -74,31 +71,27 @@ int main (int argc, char *argv[])
 }
 
 void debugDump(int * hash) {
-    for(int i; i < TRANSFORMED_SIZE; i++) {
+    for(int i = 0; i < TRANSFORMED_SIZE; i++) {
         fprintf(stdout, "%d", hash[i]);
     }
     fprintf(stdout, "\n");
-    return;
 }
 
-// complexstuff
 void imageHash(gdImagePtr image, int* bits, int label)
 {
     double map[TRANSFORMED_SIZE][TRANSFORMED_SIZE];
     double rows[TRANSFORMED_SIZE][TRANSFORMED_SIZE];
-    double mean;
 
     for (int y = 0; y < TRANSFORMED_SIZE; y++) {
-        int row[TRANSFORMED_SIZE];
+        double row[TRANSFORMED_SIZE];
         for (int x = 0; x < TRANSFORMED_SIZE; x++) {
-            int rgb = gdImageGetPixel(image, x, y);
-            row[x] = rgb;
+            row[x] = (double)gdImageGetPixel(image, x, y);
         }
         oneDimensionalDCT(row, rows[y]);
     }
 
     for (int x = 0; x < TRANSFORMED_SIZE; x++) {
-        int col[TRANSFORMED_SIZE];
+        double col[TRANSFORMED_SIZE];
         for (int y = 0; y < TRANSFORMED_SIZE; y++) {
             col[y] = rows[y][x];
         }
@@ -107,22 +100,23 @@ void imageHash(gdImagePtr image, int* bits, int label)
 
     double topPixels[TRANSFORMED_SIZE];
     int pixelCount = 0;
+
     for (int y = 0; y < sqrt(TRANSFORMED_SIZE); y++) {
         for (int x = 0; x < sqrt(TRANSFORMED_SIZE); x++) {
             topPixels[pixelCount] = map[y][x];
+            pixelCount++;
         }
     }
 
-    mean = getMeanColorFromPixelSet(topPixels);
+    double mean = getMeanColorFromPixelSet(topPixels);
 
     for(int p = 0; p < TRANSFORMED_SIZE; p++) {
         bits[p] = (topPixels[p] > mean);
     }
 
-    return;
 }
 
-void oneDimensionalDCT(int* row, double* output)
+void oneDimensionalDCT(double* row, double* output)
 {
     for(int i = 0; i < TRANSFORMED_SIZE; i++) {
         double sum = 0;
@@ -131,21 +125,20 @@ void oneDimensionalDCT(int* row, double* output)
             sum += row[j] * cos(i * M_PI * (j + 0.5) / TRANSFORMED_SIZE);
         }
 
-        sum *= sqrt(2 / TRANSFORMED_SIZE);
+        sum *= sqrt(2.0 / TRANSFORMED_SIZE);
         if(i == 0) {
             sum *= 1 / sqrt(2);
         }
         output[i] = sum;
     }
 
-    return;
 }
 
 int hammingDistance(int* one, int* two)
 {
-    int i,count = 0;
+    int count = 0;
 
-    for(i=0; i<TRANSFORMED_SIZE; i++)
+    for(int i = 0; i<TRANSFORMED_SIZE; i++)
     {
         if(one[i] != two[i])
         {
@@ -186,8 +179,7 @@ gdImagePtr resizeImage(gdImagePtr image, int w, int h)
 
 bool fileExists(char * path)
 {
-    FILE *file;
-    file = fopen(path, "r");
+    FILE *file = fopen(path, "r");
     if (file){
         fclose(file);
         return true;
@@ -196,26 +188,19 @@ bool fileExists(char * path)
 }
 
 void test() {
-    /* Image Handle*/
-    gdImagePtr image;
-    /* Output files */
-    FILE *pngout;
-    /* Color indexes */
-    int black, white;
-
     /* Allocate image memory */
-    image = gdImageCreate(64, 64);
+    gdImagePtr image = gdImageCreate(64, 64);
 
     /* Allocate colors */
-    black = gdImageColorAllocate(image, 0, 0 ,0);
-    white = gdImageColorAllocate(image, 255, 255, 255);
+    int black = gdImageColorAllocate(image, 0, 0, 0);
+    int white = gdImageColorAllocate(image, 255, 255, 255);
 
     /* Draw a line from the upper left to the lower right,
     using white color index. */
     gdImageLine(image, 0, 0, 63, 63, white);
 
     /* Open file for writing */
-    pngout = fopen("test.png", "wb");
+    FILE *pngout = fopen("test.png", "wb");
 
     /* Output file */
     gdImagePng(image, pngout);
@@ -227,5 +212,4 @@ void test() {
     gdImageDestroy(image);
 
     printf("Image created!\n");
-    return;
 }
